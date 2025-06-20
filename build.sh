79a12961037e7a06f0a7bdd9f63d08ccc0f714a6
RyanSavoia <ryanmagic11@gmail.com>
2025-06-20 08:21:16 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install Chrome
+apt-get update
+apt-get install -y wget gnupg
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+apt-get update
+apt-get install -y google-chrome-stable
+
+# Install Python dependencies
+pip install -r requirements.txt
